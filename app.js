f75b8d24abe17d0968fadd738ba0538db9e54015
mubashir-09 <mubashirkhan123muzzi@gmail.com>
2024-08-07 16:07:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+// Chapter # 01
+// Alerts
+// Question # 01
+alert("Hello");
+
+// Questin # 02
+
+alert("Hello! ThankYou To Click Our Website");
+
+// Questin # 03
+alert("This is Mubashir \n Website");
+// Chapter # 02
+// VARIABLES FOR STRINGS
+
+// Questin # 01
+let username;
+console.log(username);
+
+// Questin # 02
+let myname = "Mubashir Khan ";
+console.log(myname);
+
+// Questin # 03
+let message = "Free-Palestine";
+alert(message);
+
+// Questin # 04
+let name = "Mubashir Khan";
+let myage = "19";
+let course = "Web Development";
+alert(name);
+alert(myage);
+alert(course);
+
+// Questin # 05
+let FastFood = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(FastFood);
+
+// Questin # 06
+let Email = "mubashirkhan123muzzi@gmail.com";
+alert("Contact Email Is " + Email);
+
+// Questin # 07
+let Javabook = "A smarter way to learn JavaScript";
+alert(Javabook);
+
+// Questin # 08
+
+// Questin # 09
+let style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(style);
+
+// Chapter # 03
+// VARIABLES FOR NUMBERS
+
+// Question # 01
+let age = "I am 19 Years Old ";
+alert(age);
+
+// Question # 02
+let user = "You Visited This Website 19 Times ";
+alert(user);
+
+// Question # 03
+// let birthYear = "2004";
+
+// Question # 04
+
+// Chapter # 04
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Chapter # 05
+//MATH EXPRESSIONS
